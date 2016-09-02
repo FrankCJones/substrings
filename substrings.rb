bba7b72e0d78898b0eb301343bd3fcf90935bdee
Frank C Jones <me@frankcjones.com>
2016-09-02 00:49:08 +0000

    substrings method works with single words and fragments

--- substrings.rb
+++ substrings.rb
@@ -1,7 +1,18 @@
 def substrings(string, dictionary)
     strings = string.split(" ")
     dict = Hash[dictionary.map {|word| [word,0]}]
-    
+    strings.each do |word|
+        dict[word] += 1
+        word.split(//).each_with_index do |character,i|
+            if dict.key?(word[(i + 1)..-1])
+                dict[word[(i + 1)..-1]] +=1
+            end
+            if dict.key?(word[0..(-2-i)])
+                dict[word[0..(-2-i)]] +=1
+            end
+        end
+    end
+
     return dict.select{|k,v| v > 0}
 end
 
