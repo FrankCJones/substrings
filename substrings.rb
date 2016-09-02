350b6de84f342db93a7a354903f602b97dc66c09
Frank C Jones <me@frankcjones.com>
2016-09-02 00:07:06 +0000

    defined method and return

--- substrings.rb
+++ substrings.rb
@@ -1,3 +1,9 @@
 def substrings(string, dictionary)
+    strings = string.split(" ")
+    dict = Hash[dictionary.map {|word| [word,0]}]
     
+    return dict.select{|k,v| v > 0}
 end
+
+test_dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+substrings("below", test_dictionary)
